701d800b7f267cb7dcfadfe345289655775d986a
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2019-09-21 18:28:44 +0900

    add custom Actionbar

--- app/src/main/java/com/example/pcsp/MainActivity.java
+++ app/src/main/java/com/example/pcsp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.pcsp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setCustomActionbar();
+    }
+
+    private void setCustomActionbar(){
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // Custom Actionbar를 사용하기 위해 CustomEnable을 true로 설정, 필요없는 것은 False로 제거
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+
+        // Set Custom View Layout
+        View mCustomView = LayoutInflater.from(this).inflate(R.layout.actionbar_main,null);
+        actionBar.setCustomView(mCustomView);
+
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+
+        Toolbar parent = (Toolbar)mCustomView.getParent();
+        parent.setContentInsetsAbsolute(0,0);
+
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+        actionBar.setCustomView(mCustomView, params);
+
     }
 }
