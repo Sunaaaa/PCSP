afb5c4eb5ed2a4d587fce90d4d95f10b6994fc5f
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2019-09-21 23:14:58 +0900

    add google map API (ing)

--- app/src/main/java/com/example/pcsp/MainActivity.java
+++ app/src/main/java/com/example/pcsp/MainActivity.java
@@ -4,19 +4,48 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setCustomActionbar();
+        //setCustomActionbar();
+
+        Button btn = (Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                ComponentName componentName = new ComponentName("com.example.pcsp","com.example.pcsp.MapsActivity");
+                intent.setComponent(componentName);
+                intent.putExtra("check", "check");
+                startActivity(intent);
+                Log.i("_MAINACTIVITY","화면 이동");
+            }
+        });
+        //SupportMapFragment mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        //mMapFragment.getMapAsync(this);
+
     }
 
     private void setCustomActionbar(){
@@ -40,6 +69,23 @@ public class MainActivity extends AppCompatActivity {
 
         ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
         actionBar.setCustomView(mCustomView, params);
+
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng ME = new LatLng(37.56, 236.97);
+        Log.i("_MAINACTIVITY","Google Map");
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(ME);
+        markerOptions.title("서울");
+        markerOptions.snippet("한국의 수도");
+        mMap.addMarker(markerOptions);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ME));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
 
     }
 }
