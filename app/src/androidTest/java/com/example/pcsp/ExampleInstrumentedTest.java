d2e797d04e5b7bd733cefcf5b721ae99933e8491
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2019-09-21 15:02:14 +0900

    Initial commit

---
+++ app/src/androidTest/java/com/example/pcsp/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.example.pcsp;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(JUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.example.pcsp", appContext.getPackageName());
+    }
+}
