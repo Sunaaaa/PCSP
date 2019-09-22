bb5d779241c86e845c4826ae02192fc85b708f21
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2019-09-22 22:34:50 +0900

    add UserVO

---
+++ app/src/main/java/com/example/pcsp/UserVO.java
@@ -0,0 +1,39 @@
+package com.example.pcsp;
+
+public class UserVO {
+    int userNum;
+    String userName;
+    String userId;
+    String userPw;
+    String userPhoneNum;
+    String userAddress;
+    int areaNum;
+
+    public int getUserNum() {
+        return userNum;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserPw() {
+        return userPw;
+    }
+
+    public String getUserPhoneNum() {
+        return userPhoneNum;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public int getAreaNum() {
+        return areaNum;
+    }
+}
