bf628ebfe056f4dab1dcd916351c3471d6fdd3d3
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2019-09-22 22:43:24 +0900

    add CarVO

---
+++ app/src/main/java/com/example/pcsp/CarVO.java
@@ -0,0 +1,76 @@
+package com.example.pcsp;
+
+public class CarVO {
+    String carNum;
+    int carTypeNum;
+    String carLong;
+    String carLat;
+    String carStatus;
+    int carOwnerNum;
+    String carRentFee;
+    String carHipassFee;
+
+    public String getCarNum() {
+        return carNum;
+    }
+
+    public void setCarNum(String carNum) {
+        this.carNum = carNum;
+    }
+
+    public int getCarTypeNum() {
+        return carTypeNum;
+    }
+
+    public void setCarTypeNum(int carTypeNum) {
+        this.carTypeNum = carTypeNum;
+    }
+
+    public String getCarLong() {
+        return carLong;
+    }
+
+    public void setCarLong(String carLong) {
+        this.carLong = carLong;
+    }
+
+    public String getCarLat() {
+        return carLat;
+    }
+
+    public void setCarLat(String carLat) {
+        this.carLat = carLat;
+    }
+
+    public String getCarStatus() {
+        return carStatus;
+    }
+
+    public void setCarStatus(String carStatus) {
+        this.carStatus = carStatus;
+    }
+
+    public int getCarOwnerNum() {
+        return carOwnerNum;
+    }
+
+    public void setCarOwnerNum(int carOwnerNum) {
+        this.carOwnerNum = carOwnerNum;
+    }
+
+    public String getCarRentFee() {
+        return carRentFee;
+    }
+
+    public void setCarRentFee(String carRentFee) {
+        this.carRentFee = carRentFee;
+    }
+
+    public String getCarHipassFee() {
+        return carHipassFee;
+    }
+
+    public void setCarHipassFee(String carHipassFee) {
+        this.carHipassFee = carHipassFee;
+    }
+}
