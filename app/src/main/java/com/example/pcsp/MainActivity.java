5450d55ef84145059c408abea89c9b360c7ccca2
Sunah Gong <glglthssla@naver.com>
2019-09-23 11:16:29 +0900

    set custom actionbar

--- app/src/main/java/com/example/pcsp/MainActivity.java
+++ app/src/main/java/com/example/pcsp/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //setCustomActionbar();
+        setCustomActionbar();
 
         Button btn = (Button)findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
